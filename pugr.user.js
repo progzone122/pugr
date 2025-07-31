c0a47f4456ec283cab49565c72a4889868142188
diablo <progzone122@gmail.com>
2025-07-31 18:56:17 +0300

    .

---
+++ pugr.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         PhotopeaUselessGapRemover (PUGR)
+// @namespace    https://github.com/progzone122/pugr
+// @version      2025-07-31
+// @description  Removes the right gap (block) in Photopea used to display ads, expanding the workspace
+// @author       DiabloSat (progzone122)
+// @match        https://www.photopea.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=photopea.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    if (!Object.getOwnPropertyDescriptor(window, 'innerWidth')?.configurable) {
+        console.warn('innerWidth is not configurable. Skipping.');
+    } else {
+        Object.defineProperty(window, 'innerWidth', {
+            get() { return document.documentElement.offsetWidth + 320 },
+            configurable: true
+        });
+    }
+})();
