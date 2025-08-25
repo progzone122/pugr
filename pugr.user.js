869bc8150deb3721687449bd8b0de9d8d99d3cc5
Adrián Cancio <49328080+adrian-cancio@users.noreply.github.com>
2025-08-26 00:48:42 +0200

    Update version, match URL, update URL and download URL in user script
    - Updated @match to match any URLs (Ex. refs URLs).
    - Added @updateURL and @DownloadURL.

--- pugr.user.js
+++ pugr.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         PhotopeaUselessGapRemover (PUGR)
 // @namespace    https://github.com/progzone122/pugr
-// @version      2025-07-31
+// @version      2025-08-26
 // @description  Removes the right gap (block) in Photopea used to display ads, expanding the workspace
 // @author       DiabloSat (progzone122)
-// @match        https://www.photopea.com/
+// @match        https://www.photopea.com/*
 // @icon         https://raw.githubusercontent.com/progzone122/pugr/refs/heads/main/logo.webp
+// @updateURL    https://raw.githubusercontent.com/progzone122/pugr/refs/heads/main/pugr.user.js
+// @downloadURL  https://raw.githubusercontent.com/progzone122/pugr/refs/heads/main/pugr.user.js
 // @grant        none
 // ==/UserScript==
 
